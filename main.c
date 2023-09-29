a8ee1b28e7d6c5f993dcf20a48ecd2ef801a4296
Rob Johnson <johnson.rob.alan@gmail.com>
2023-09-28 21:46:21 -0700

    read the hardware quadrature decoder

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "lcd.h"
 #include "project.h"
 
+
+
 int main(void) {
     lcd_init();
     lcd_print_line_str(0, "Rotary Encoder");
 
+    QuadDec_1_Start();
+    
     while(1) {
+        lcd_print_line_str_int16(1, "encoder1", QuadDec_1_GetCounter());
+        
+        CyDelay(200);
     }
 }
